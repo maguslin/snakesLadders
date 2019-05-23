6b5654b26230956676e5cb4e8b785a3026bdfffc
seanyoungIOM <seanyoung@online.sch.im>
2019-05-23 08:58:53 +0100

    Playing around to see what works

--- OOPSnamkes/OOPSnamkes/Form1.cs
+++ OOPSnamkes/OOPSnamkes/Form1.cs
@@ -21,7 +21,44 @@ namespace OOPSnamkes
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Bitmap background = DrawBoard();
+
+            PictureBox gameBoardDisplay = new PictureBox();
+            gameBoardDisplay.Height = this.Height;
+            gameBoardDisplay.Width = this.Width * (2 / 3);
+            gameBoardDisplay.Location = new Point(0, 0);
+            gameBoardDisplay.Image = background;
+
+            TextBox txtbx = new TextBox();
+            txtbx.Text = "";
+            txtbx.Name = "txtbx1";
+            txtbx.Location = new Point(10, 10);
+            txtbx.Height = 20;
+            txtbx.Width = 50;
+            Controls.Add(txtbx);
+
+            this.Text = "help";
+
+            this.Controls.Add(gameBoardDisplay);
+            
+
+
+
+
+
             game.PlayGame();
+        }
+
+
+        private Bitmap DrawBoard()
+        {
+            Bitmap board = new Bitmap(Properties.Resources.snlboard001, Properties.Resources.snlboard001.Height, Properties.Resources.snlboard001.Width);
+            using(Graphics g = Graphics.FromImage(board))
+            {
+                g.DrawImage(Properties.Resources.s_l, new Point[] { new Point(0, 0), new Point(board.Width, 0), new Point(0, board.Height) });
+            }
+
+            return board;
         }
     }
 }
